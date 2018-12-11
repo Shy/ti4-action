2aa06ffd1f146e46796009bc11e75f14cc2ba59f
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-12-11 16:25:53 -0500

    removed extra fluff

--- netlify-rebuild/entrypoint.sh
+++ netlify-rebuild/entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -e
 
-prefix="refs/heads/"
-branch=${GITHUB_REF#"$prefix"}
-
-sh -c "curl $branch"
-
-if test "$branch" = 'master'; then
+if test "$GITHUB_REF" = 'refs/heads/master'; then
     sh -c "echo 'Triggering rebuild on Netlify via webhook.'"
     sh -c "curl -X POST -d {} https://api.netlify.com/build_hooks/5c101b97f880678ed409718d"
 else
