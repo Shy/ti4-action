ca5176221aa681326db6f086580fa12cf9af3ab9
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-12-11 16:09:51 -0500

    grammer

--- netlify-rebuild/entrypoint.sh
+++ netlify-rebuild/entrypoint.sh
@@ -5,9 +5,11 @@ set -e
 prefix="refs/heads/"
 branch=${GITHUB_REF#"$prefix"}
 
+sh -c "curl $branch"
+
 if test "$branch" = 'master'; then
     sh -c "echo 'Triggering rebuild on Netlify via webhook.'"
     sh -c "curl -X POST -d {} https://api.netlify.com/build_hooks/5c101b97f880678ed409718d"
 else
-    sh -c "echo 'Branch Deploy. Skipping rebuild'"
+    sh -c "echo 'Branch Deploy. Skipping rebuild.'"
 fi
