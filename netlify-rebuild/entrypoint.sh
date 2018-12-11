6b00da1d11cca6ed703a04464851c9de7532b01e
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-12-11 16:07:10 -0500

    Added if test if not master

--- netlify-rebuild/entrypoint.sh
+++ netlify-rebuild/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-sh -c "echo 'Triggering rebuild on Netlify via webhook.'"
-sh -c "curl -X POST -d {} https://api.netlify.com/build_hooks/5c101b97f880678ed409718d"
+
+if test "$branch" = 'master'; then
+    sh -c "echo 'Triggering rebuild on Netlify via webhook.'"
+    sh -c "curl -X POST -d {} https://api.netlify.com/build_hooks/5c101b97f880678ed409718d"
+else
+    sh -c "echo 'Branch Deploy. Skipping rebuild'"
+fi
