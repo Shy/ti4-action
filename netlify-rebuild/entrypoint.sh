ac2915cd5afc01d47f94327357f3117e271ccda5
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-12-11 16:39:56 -0500

    destroyed buildhookURL and moved it into secrets

--- netlify-rebuild/entrypoint.sh
+++ netlify-rebuild/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if test "$GITHUB_REF" = 'refs/heads/master'; then
     sh -c "echo 'Triggering rebuild on Netlify via webhook.'"
-    sh -c "curl -X POST -d {} https://api.netlify.com/build_hooks/5c101b97f880678ed409718d"
+    sh -c "curl -X POST -d {} $netlifyBuildHook"
 else
     sh -c "echo 'Branch Deploy. Skipping rebuild.'"
 fi
