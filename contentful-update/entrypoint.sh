efeb38c9dba128335f8df257fdf6bb479798548a
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-12-11 15:20:44 -0500

    updated webhook thing

--- contentful-update/entrypoint.sh
+++ contentful-update/entrypoint.sh
@@ -8,7 +8,8 @@ branch=${GITHUB_REF#"$prefix"}
 npm install
 
 if test "$branch" = 'master'; then
-    curl -X POST -d {} https://api.netlify.com/build_hooks/5c101b97f880678ed409718d
+    sh -c "echo Deploying on Netlify"
+    sh -c "curl -X POST -d {} https://api.netlify.com/build_hooks/5c101b97f880678ed409718d"
 else
     sh -c "echo branch"
     sh -c "echo $branch"
