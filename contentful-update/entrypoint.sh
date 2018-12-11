e6e9a426305060f2cdab0f8854292622096c20d3
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-12-11 12:31:28 -0500

    refactor

--- contentful-update/entrypoint.sh
+++ contentful-update/entrypoint.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-if [ -z "$GITHUB_REF" == 'refs/heads/master' ]; then
+prefix="refs/heads/"
+branch=${GITHUB_REF#"$prefix"}
+
+if [ -z "$branch" = 'master' ]; then
     sh -c "echo master"
 else
     sh -c "echo branch"
-    prefix="refs/heads/"
-    branch=${GITHUB_REF#"$prefix"}
     sh -c "echo $branch"
 fi
