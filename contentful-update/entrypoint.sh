24c2cc6c7f49a23a4fedc30650213a29ae1454ac
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-12-11 12:59:37 -0500

    Initilize Migration Commit

--- contentful-update/entrypoint.sh
+++ contentful-update/entrypoint.sh
@@ -5,9 +5,13 @@ set -e
 prefix="refs/heads/"
 branch=${GITHUB_REF#"$prefix"}
 
+npm install
+
 if test "$branch" = 'master'; then
     sh -c "echo master"
 else
     sh -c "echo branch"
     sh -c "echo $branch"
 fi
+
+migrate.js $spaceId $branch $accessToken
