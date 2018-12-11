ed48fe8f9379d80e11a656e59781b66b50231882
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-12-11 15:19:24 -0500

    add build hook

--- contentful-update/entrypoint.sh
+++ contentful-update/entrypoint.sh
@@ -8,10 +8,8 @@ branch=${GITHUB_REF#"$prefix"}
 npm install
 
 if test "$branch" = 'master'; then
-    sh -c "echo master"
+    curl -X POST -d {} https://api.netlify.com/build_hooks/5c101b97f880678ed409718d
 else
     sh -c "echo branch"
     sh -c "echo $branch"
 fi
-
-migrate.js $spaceId $branch $accessToken
